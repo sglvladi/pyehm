e3776504728b5273d748e1cc6521cd46241d518d
sglvladi <sglvladi@gmail.com>
2021-09-17 02:01:34 +0100

    Fixed invalid classifier and updated info in setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,17 @@ setup(
     name="pyehm",
     version=version,
     author="Lyudmil Vladimirov",
-    author_email="sglvladi@gmail.com",
+    author_email="sglvladi@liverpool.ac.uk",
     maintainer="University of Liverpool",
     url='https://github.com/sglvladi/pyehm',
-    description='Efficient Hypothesis Management (EHM) Python Implementation',
+    description='Python Efficient Hypothesis Management (PyEHM)',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    project_urls={
+        'Documentation': 'https://pyehm.rtfd.io/',
+        'Source': 'https://github.com/sglvladi/pyehm',
+        'Issue Tracker': 'https://github.com/sglvladi/pyehm/issues'
+    },
     packages=find_packages(exclude=('docs', '*.tests')),
     setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
     install_requires=['numpy', 'networkx', 'stonesoup', 'setuptools>=42', 'pydot', 'matplotlib'],
@@ -30,7 +35,7 @@ setup(
     keywords=['python', 'pyehm', 'ehm'],
     classifiers=[
         "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)"
+        "License :: OSI Approved :: Eclipse Public License 2.0 (EPL-2.0)",
         "Intended Audience :: Science/Research",
         "Topic :: Scientific/Engineering",
         "Programming Language :: Python :: 3 :: Only",
